72c3febdf69bc6a2d720e11c5d79df79ba33b820
lishaoshi <1332456550@qq.com>
2021-09-03 14:30:43 +0800

    invalid key

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import AgfridCn from './components/ag-grid-cn'
 
 import './style/index.scss'
 
-import { LicenseManager } from '@ag-grid-enterprise/core'
-LicenseManager.setLicenseKey(`CompanyName=Shenzhen Huitongguan Network Technology Co., Ltd,LicensedGroup=Shenzhen Huitongguan Network Technology Co., Ltd,LicenseType=MultipleApplications,LicensedConcurrentDeveloperCount=1,LicensedProductionInstancesCount=1,AssetReference=AG-017061,ExpiryDate=5_July_2022_[v2]_MTY1Njk3NTYwMDAwMA==7d62b8378360a3402254d4cd22f59109`)
-
-
 
 const components = [
   AgfridCn
